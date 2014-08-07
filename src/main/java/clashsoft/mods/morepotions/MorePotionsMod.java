cc36f3265a0ccc227157bee564694702df8f01c5
Clashsoft <clashsoft@hotmail.com>
2014-08-07 09:21:21 +0200

    1.7.10-1.0.2

--- src/main/java/clashsoft/mods/morepotions/MorePotionsMod.java
+++ src/main/java/clashsoft/mods/morepotions/MorePotionsMod.java
@@ -51,7 +51,7 @@ public class MorePotionsMod extends ClashsoftMod<MPMNetHandler>
 	public static final String				NAME						= "More Potions Mod";
 	public static final String				ACRONYM						= "mpm";
 	public static final String				DEPENDENCIES				= CSLib.DEPENDENCY + ";required-after:" + BrewingAPI.MODID;
-	public static final String				VERSION						= CSUpdate.CURRENT_VERSION + "-1.0.1";
+	public static final String				VERSION						= "1.7.10-1.0.2";
 	
 	@Instance(MODID)
 	public static MorePotionsMod			instance;
